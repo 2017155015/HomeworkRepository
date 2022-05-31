0b636f102cd6288df7c8df8616ed168ef627f3ce
yunyoungjae <geniusyun98@naver.com>
2022-06-01 03:50:32 +0900

    Update script.js

--- LAB6/script.js
+++ LAB6/script.js
@@ -124,3 +124,62 @@ function initialize(products) {
         section.appendChild(img);
     }
 }
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if(clientHeight + scrollTop >= scrollHeight - 3) {
+        load();
+    }
+});
+
+let counter = 4;
+
+function load() {
+    const main = document.querySelector('main');
+
+    var start = counter;
+    var end = start + 4;
+    counter = end;
+
+    fetch(myRequest).then(response => response.json()).then(function(json) {
+        let products = json;
+        for(start; start < end; start++) {
+            const section = document.createElement('section');
+            const img = document.createElement('img');
+            const container = document.createElement('div');
+            const info = document.createElement('p');
+            const price = document.createElement('p');
+
+            let url = './images/' + products[start].img;
+    
+            section.setAttribute('class', 'onClickTextOverImage');
+
+            container.setAttribute('class', 'clickable');
+            container.id = start;
+            container.style.opacity = "0";
+            container.onclick = function(){
+                var x = document.getElementById(this.id);
+                if(x.style.opacity === "0"){
+                    x.style.opacity = "1";
+                } else if(x.style.opacity === "1"){
+                    x.style.opacity = "0";
+                } else {
+                    x.style.opacity = "0";
+                }
+            }
+
+            info.textContent = products[start].name;
+            price.textContent = products[start].price + '₩';
+            
+            img.src = url;
+            img.alt = products[start].name;
+    
+            main.appendChild(section);
+            section.appendChild(container);
+            container.appendChild(info);
+            container.appendChild(price);
+            section.appendChild(img);
+        }
+    })
+        .catch(console.error);
+};
