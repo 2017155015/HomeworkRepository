f49c200df568dad7fdb3ada1727810b173f4660c
yunyoungjae <geniusyun98@naver.com>
2022-06-06 11:11:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 const express = require("express");
 
+const fs = require("fs");
+
 const app = express();
+
+app.post("/write-file", function (req, res) {
+    console.log(req.body);
+
+    if (!req.body?.content) {
+        res.status(400).send("400 에러! content가 post body에 없습니다!");
+        return;
+    }
+
+    fs.writeFile("test.txt", req.body.content, function (error, data) {
+        if (error) {
+            res.status(500).send("500 서버 에러!");
+        } else {
+            res.status(201).send("파일 생성 성공!");
+        }
+    });
+});
+
+app.get("/read-file", function (req, res) {
+    console.log(req.query);
+
+    if (!req.query?.file) {
+        res.status(400).send("400 에러! file이 query parameters에 없습니다.");
+        return;
+    }
+
+    fs.readFile(req.query.file, "utf-8", async function (error, data) {
+        if (error) {
+            if (error.code == "ENOENT") {
+                res.status(404).send('${req.query.file}이 없습니다.');
+            } else {
+                res.status(500).send("500 서버 에러!");
+            }
+        } else {
+            res.status(200).send(data.toString());
+        }
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World~!");
@@ -14,6 +54,8 @@ app.get("/posts", function (req, res) {
 })
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8000;
 
